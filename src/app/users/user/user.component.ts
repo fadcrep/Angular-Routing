196acca2f8cb232aa980f4a5028820a83a1edf08
Crepin Fadjo <crepin.fadjo@qanbio.com>
2018-01-08 11:07:10 +0100

    queryParams and fragment in route

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute,Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 
 @Component({
@@ -7,8 +8,9 @@ import { ActivatedRoute,Params } from '@angular/router';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription : Subscription
 
   constructor(private route: ActivatedRoute) { }
 
@@ -19,13 +21,18 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['name']
     }
 
-    this.route.params
+    this.paramsSubscription = this.route.params
     .subscribe(
       (params:Params) =>{
         this.user.id=params['id'];
         this.user.name=params['name'];
       }
     );
+
+  }
+
+  ngOnDestroy(){
+    this.paramsSubscription.unsubscribe();
 
   }
 
